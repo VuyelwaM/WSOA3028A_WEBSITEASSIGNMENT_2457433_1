4e66c9cd711726883f1caf3bc8964566119e0772
VuyelwaM <116339913+VuyelwaM@users.noreply.github.com>
2023-04-17 13:17:34 +0200

    Visual Studio Code
    Created Respository

---
+++ about.js
@@ -0,0 +1,99 @@
+function showBlogPost1() {
+    var post = document.getElementById("blog-post-1");
+    post.style.display = "block";
+  }
+
+  function toggleBlogPost1() {
+    var post = document.getElementById("blog-post-1");
+    var button = document.getElementById("toggle-button");
+  
+    if (post.style.display === "none") {
+      post.style.display = "block";
+      button.innerText = "Show less";
+    } else {
+      post.style.display = "none";
+      button.innerText = "Show more";
+    }
+  }
+  
+  
+  function showBlogPost2() {
+    var post = document.getElementById("blog-post-2");
+    post.style.display = "block";
+  }
+  function toggleBlogPost2() {
+    var post = document.getElementById("blog-post-2");
+    var button = document.getElementById("toggle-button");
+  
+    if (post.style.display === "none") {
+      post.style.display = "block";
+      button.innerText = "Show less";
+    } else {
+      post.style.display = "none";
+      button.innerText = "Show more";
+    }
+  }
+  function showBlogPost3() {
+    var post = document.getElementById("blog-post-3");
+    post.style.display = "block";
+  }
+  function toggleBlogPost3() {
+    var post = document.getElementById("blog-post-3");
+    var button = document.getElementById("toggle-button");
+  
+    if (post.style.display === "none") {
+      post.style.display = "block";
+      button.innerText = "Show less";
+    } else {
+      post.style.display = "none";
+      button.innerText = "Show more";
+    }
+  }
+  function showBlogPost4() {
+    var post = document.getElementById("blog-post-4");
+    post.style.display = "block";
+  }
+  function toggleBlogPost4() {
+    var post = document.getElementById("blog-post-4");
+    var button = document.getElementById("toggle-button");
+  
+    if (post.style.display === "none") {
+      post.style.display = "block";
+      button.innerText = "Show less";
+    } else {
+      post.style.display = "none";
+      button.innerText = "Show more";
+    }
+  }
+  function showBlogPost5() {
+    var post = document.getElementById("blog-post-5");
+    post.style.display = "block";
+  }
+  function toggleBlogPost5() {
+    var post = document.getElementById("blog-post-5");
+    var button = document.getElementById("toggle-button");
+  
+    if (post.style.display === "none") {
+      post.style.display = "block";
+      button.innerText = "Show less";
+    } else {
+      post.style.display = "none";
+      button.innerText = "Show more";
+    }
+  }
+  function showBlogPost6() {
+    var post = document.getElementById("blog-post-6");
+    post.style.display = "block";
+  }
+  function toggleBlogPost6() {
+    var post = document.getElementById("blog-post-6");
+    var button = document.getElementById("toggle-button");
+  
+    if (post.style.display === "none") {
+      post.style.display = "block";
+      button.innerText = "Show less";
+    } else {
+      post.style.display = "none";
+      button.innerText = "Show more";
+    }
+  }
